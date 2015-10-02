d77364af0816703b1ac34f71a92dd2788adf6bd0
Martin Elwin <elwinm@amazon.com>
2015-10-02 12:06:58 +0200

    Misc fixes.
    Use built in document client.
    Add error handling.
    Do proper batch writes against DynamoDB and CloudWatch.

--- iot_ddb_cw_eventprocessor.js
+++ iot_ddb_cw_eventprocessor.js
@@ -3,46 +3,65 @@
 console.log('Loading function');
 
 var AWS = require('aws-sdk');
-var DOC = require('dynamodb-doc');
 
 var cw = new AWS.CloudWatch();
-var doc = new DOC.DynamoDB();
+var doc = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function(event, context) {
     console.log('Received event:', JSON.stringify(event, null, 2));
+
+    var stackName = context.functionName.split("-")[0];
+
+    var ddbParams = {
+        RequestItems: {}
+    };
+
+    var putItems = [];
+
+    var cwParams = {
+        MetricData: [],
+        Namespace: 'Sensors'
+    };
 
     event.Records.forEach(function(record) {
         payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
         console.log('Decoded payload:', payload);
 
-        update(JSON.parse(payload), context);
+        var sensorEvent = JSON.parse(payload);
+
+        putItems.push(
+            {
+                PutRequest: {
+                    Item: {
+                        SensorId: sensorEvent.sensorid,
+                        Timestamp: sensorEvent.timestamp,
+                        Value: sensorEvent.value
+                    }
+                }
+            });
+
+        cwParams.MetricData.push(
+            {
+                MetricName: 'SensorData',
+                Dimensions: [{ Name: 'SensorId', Value: sensorEvent.sensorid }],
+                Timestamp: new Date(sensorEvent.timestamp),
+                Value: sensorEvent.value
+            });
     });
-};
 
-var update = function(event, context) {
+    ddbParams.RequestItems[stackName + '-SensorData'] = putItems;
 
-    var stackName = context.functionName.split("-")[0];
-
-    var itemParams = {
-        TableName: stackName + '-SensorData',
-        Item: {
-            SensorId: event.sensorid,
-            Timestamp: event.timestamp,
-            Value: event.value
-        }
-    };
-
-    doc.putItem(itemParams, function(err, data) {
+    doc.batchWrite(ddbParams, function(err, data) {
         if (err) {
             console.log('DDB call failed: ' + err, err.stack);
-            context.error(err);
+            context.fail(err);
         } else {
 
             //if all is well - also log the metric to CloudWatch
-            logMetric(event, function(err, data) {
+            cw.putMetricData(cwParams, function(err, data) {
                 if (err) {
                     console.log('CW call failed: ' + err, err.stack);
-                    context.error(err);
+                    context.fail(err);
                 } else {
                     context.succeed();
                 }
@@ -51,16 +70,3 @@ var update = function(event, context) {
     });
 };
 
-var logMetric = function(event, callback) {
-    var params = {
-        MetricData: [{
-            MetricName: 'SensorData',
-            Dimensions: [{ Name: 'SensorId', Value: event.sensorid }],
-            Timestamp: new Date(event.timestamp),
-            Value: event.value
-        }],
-        Namespace: 'Sensors'
-    };
-
-    cw.putMetricData(params, callback);
-};
